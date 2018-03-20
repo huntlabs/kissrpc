7ce37ba9bc30e45aeaac7aa0e06922763a72c31b
noclear <noclear@163.com>
2018-03-20 13:51:03 +0800

    tmp remove rpcgenerate dir

--- examples/client/source/rpcgenerate/greeter/package.d
+++
@@ -1,8 +0,0 @@
-
-
-module rpcgenerate.greeter;
-
-public import rpcgenerate.greeter.Greeter;
-public import rpcgenerate.greeter.GreeterRequestFb;
-public import rpcgenerate.greeter.GreeterResponseFb;
-public import rpcgenerate.greeter.GreeterStub;
